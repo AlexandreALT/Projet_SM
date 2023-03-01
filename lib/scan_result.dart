4be88b2cfc63012c2ede78be0680c85dec3a6b53
AlexandreALT <alex.ant2510@gmail.com>
2023-03-01 14:43:51 +0100

    Images resultat scan + diff ajout produit

--- lib/scan_result.dart
+++ lib/scan_result.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:projet_sm/models/product.dart';
 import 'package:projet_sm/tools/menu.dart';
-import 'package:projet_sm/tools/search_bar.dart';
 
 import 'affect_site.dart';
 
@@ -14,138 +14,149 @@ class ScanResult extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: Icon(Icons.menu),
-              color: Colors.black,
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-            );
-          },
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        title: const Text(
-          "Résultat de la recherche",
-          style: TextStyle(color: Colors.black, fontSize: 20),
-        ),
-      ),
-      drawer: Menu(),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: ListView(
-          children: <Widget>[
-            Container(
-              height: MediaQuery.of(context).size.height - 500,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    Text(
-                      product.reference,
-                      style: TextStyle(fontSize: 25),
-                    ),
-                    Text(product!.numeroSerie!, style: TextStyle(fontSize: 20)),
-                    //Image.file(
-                      //File(product.image),
-                    //),
-                  ],
-                ),
+    final ref = FirebaseStorage.instance.ref().child(product.image);
+    return FutureBuilder<String>(
+        future: ref.getDownloadURL(),
+        builder: (context, snapshot) {
+          return Scaffold(
+            appBar: AppBar(
+              leading: Builder(
+                builder: (BuildContext context) {
+                  return IconButton(
+                    icon: Icon(Icons.menu),
+                    color: Colors.black,
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                  );
+                },
+              ),
+              centerTitle: true,
+              backgroundColor: Colors.white,
+              title: const Text(
+                "Résultat de la recherche",
+                style: TextStyle(color: Colors.black, fontSize: 20),
               ),
             ),
-            SizedBox(height: MediaQuery.of(context).size.height / 12),
-            Text(product.image),
-            Image.file(
-              File(product.image),
-              fit: BoxFit.cover,
+            drawer: Menu(),
+            body: Padding(
+              padding: const EdgeInsets.all(20),
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            product.reference,
+                            style: TextStyle(fontSize: 25),
+                          ),
+                          Text(product!.numeroSerie!,
+                              style: TextStyle(fontSize: 20)),
+                        ],
+                      ),
+                    ),
+                  ),
+                  snapshot.data == null
+                      ? Text('Aucune image pour ce produit')
+                      : Image.network(
+                          snapshot.data!,
+                          height: MediaQuery.of(context).size.height / 2,
+                        ),
+                  SizedBox(height: 10,),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        height: 50,
+                        width: MediaQuery.of(context).size.width - 40,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50.0)),
+                            ),
+                          ),
+                          onPressed: () async {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        AffectSite(produit: product)));
+                          },
+                          child: const Text(
+                            'Affecter à un chantier',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height / 40),
+                      Container(
+                        height: 50,
+                        width: MediaQuery.of(context).size.width - 40,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50.0)),
+                            ),
+                          ),
+                          onPressed: () async {
+                            //Navigator.pushNamed(context, '/choice_capture');
+                          },
+                          child: const Text(
+                            'Retour de produit',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height / 40),
+                      Container(
+                        height: 50,
+                        width: MediaQuery.of(context).size.width - 40,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50.0)),
+                            ),
+                          ),
+                          onPressed: () async {
+                            Navigator.pushNamed(context, '/choice_capture');
+                          },
+                          child: const Text(
+                            'Re-scanner',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height / 40),
+                      Container(
+                        height: 50,
+                        width: MediaQuery.of(context).size.width - 40,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50.0)),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/accueil');
+                          },
+                          child: const Text(
+                            'Retour à l\'accueil',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            Column(
-              children: <Widget>[
-                Container(
-                  height : 50,
-                  width: MediaQuery.of(context).size.width-40,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                      ),
-                    ),
-                    onPressed: () async {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => AffectSite(produit: product)));
-                    },
-                    child: const Text(
-                      'Affecter à un chantier',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height / 40),
-                Container(
-                  height : 50,
-                  width: MediaQuery.of(context).size.width-40,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                      ),
-                    ),
-                    onPressed: () async {
-                      //Navigator.pushNamed(context, '/choice_capture');
-                    },
-                    child: const Text(
-                      'Retour de produit',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height / 40),
-                Container(
-                  height : 50,
-                  width: MediaQuery.of(context).size.width-40,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                      ),
-                    ),
-                    onPressed: () async {
-                      Navigator.pushNamed(context, '/choice_capture');
-                    },
-                    child: const Text(
-                      'Re-scanner',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height / 40),
-                Container(
-                  height : 50,
-                  width: MediaQuery.of(context).size.width-40,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                      ),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/accueil');
-                    },
-                    child: const Text(
-                      'Retour à l\'accueil',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
---
+++ lib/stock/form_others.dart
@@ -0,0 +1,144 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+import 'package:projet_sm/Services/productDB.dart';
+import 'package:projet_sm/Services/referenceDB.dart';
+import 'package:projet_sm/models/product.dart';
+
+class FormOthers extends StatelessWidget {
+  FormOthers({Key? key, this.image}) : super(key: key);
+
+  final image;
+
+  var refcontroller = TextEditingController();
+  var quantitecontroller = TextEditingController();
+  var categoriecontroller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(children: [
+        Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.25,
+            width: MediaQuery.of(context).size.width * 0.4,
+            color: Color.fromRGBO(232, 232, 232, 1.0),
+            child: image == null
+                ? IconButton(
+                    icon: Icon(Icons.add_a_photo, color: Colors.black),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(
+                          context, '/choice_picture');
+                    },
+                  )
+                : Image.file(
+                    File(image.path),
+                    fit: BoxFit.cover,
+                  ),
+          ),
+        ),
+        SizedBox(height: 20),
+        TextFormField(
+          controller: refcontroller,
+          keyboardType: TextInputType.text,
+          style: const TextStyle(color: Colors.black),
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+                borderSide: BorderSide(
+                  width: 0,
+                  style: BorderStyle.none,
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(50.0))),
+            filled: true,
+            fillColor: Color.fromRGBO(232, 232, 232, 1.0),
+            hintText: 'Référence',
+            suffixIcon: IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.pushNamed(context, '/add_reference');
+              },
+            ),
+          ),
+        ),
+        SizedBox(height: MediaQuery.of(context).size.height / 6),
+        Column(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                  ),
+                ),
+                onPressed: () async {
+                  var reference = refcontroller.text;
+
+                  var ref = await ReferenceDB().getReference(reference);
+                  var serie = ref["compteur"].toString() + ref["alias"];
+                  await ReferenceDB().updateReference(
+                      reference, 'compteur', ref["compteur"] + 1);
+
+                  var imgPath = "Products/" +
+                      ref["nom"] +
+                      "/" +
+                      ref["reference"] +
+                      "/" +
+                      serie +
+                      ".jpg";
+
+                  final storageRef =
+                      FirebaseStorage.instance.ref().child(imgPath);
+                  File file = File(image.path);
+                  storageRef.putFile(file);
+
+                  String jour = DateTime.now().day.toString();
+                  String mois = DateTime.now().month.toString();
+                  String annee = DateTime.now().year.toString();
+                  if (mois.length < 2) mois = "0" + mois;
+                  if (jour.length < 2) jour = "0" + jour;
+                  String date = jour + '/' + mois + '/' + annee;
+                  var product = new Product(
+                      cout: ref["cout"],
+                      categorie: categoriecontroller.text,
+                      quantite: int.parse(quantitecontroller.text),
+                      reference: reference,
+                      image: imgPath,
+                      date_ajout: date,
+                      numeroSerie: serie,
+                      statut: "En entrepôt");
+                  await ProductDB().addProduct(product);
+                  Navigator.pushNamed(context, '/new_product');
+                },
+                child: const Text(
+                  'Ajouter au magasin',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  'Annuler',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ]),
+    );
+  }
+}
